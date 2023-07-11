957d369e7045c86b1095e117fbd12c762e62b5e8
qc <1057117849@qq.com>
2023-07-11 19:53:03 +0800

    升级了vite版本，修复了退出登录需要手动刷新的bug 修改了打包base为/

--- config/vite.config.prod.ts
+++ config/vite.config.prod.ts
@@ -1,20 +1,22 @@
-import { mergeConfig } from 'vite';
+import {mergeConfig} from 'vite';
 import baseConfig from './vite.config.base';
 import configCompressPlugin from './plugin/compress';
 import configVisualizerPlugin from './plugin/visualizer';
 import configArcoResolverPlugin from './plugin/arcoResolver';
 import configImageminPlugin from './plugin/imagemin';
 
+// @ts-ignore
+// @ts-ignore
 export default mergeConfig(
-  {
-    mode: 'production',
-    plugins: [
-      configCompressPlugin('gzip'),
-      configVisualizerPlugin(),
-      configArcoResolverPlugin(),
-      configImageminPlugin(),
-    ],
-    build: {
+    {
+        mode: 'production',
+        plugins: [
+            configCompressPlugin('gzip'),
+            configVisualizerPlugin(),
+            configArcoResolverPlugin(),
+            configImageminPlugin(),
+        ],
+        build: {
       rollupOptions: {
         output: {
           manualChunks: {
