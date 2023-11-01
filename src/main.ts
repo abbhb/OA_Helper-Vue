9019c3cf55de1c5571d1689e15a75897dbcc4ce3
Cheng Qiu <qc2003020403@163.com>
2023-11-01 20:07:31 +0800

    更换主题@arco-themes/vue-gi-demo

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import {createApp} from 'vue';
+import { createApp } from 'vue';
 import '@/utils/chat/websocket';
 import ArcoVue from '@arco-design/web-vue';
 import ArcoVueIcon from '@arco-design/web-vue/es/icon';
-import '@arco-design/web-vue/dist/arco.css';
+import '@arco-themes/vue-gi-demo/index.less';
+// 官方主题 import '@arco-design/web-vue/dist/arco.css';
 import globalComponents from '@/components';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import useRSAStore from '@/store/modules/rsa';
@@ -11,14 +12,14 @@ import store from './store';
 import i18n from './locale';
 import directive from './directive';
 import './mock';
-import 'highlight.js/styles/atom-one-dark.css' // 样式
+import 'highlight.js/styles/atom-one-dark.css'; // 样式
 import App from './App.vue';
 // Styles are imported via arco-plugin. See config/plugin/arcoStyleImport.ts in the directory for details
 // 样式通过 arco-plugin 插件导入。详见目录文件 config/plugin/arcoStyleImport.ts
 // https://arco.design/docs/designlab/use-theme-package
 import '@/assets/style/global.less';
 import '@imengyu/vue3-context-menu/lib/vue3-context-menu.css';
-  import '@/api/interceptor';
+import '@/api/interceptor';
 
 // 会发现这样不行
 // if (!isT()) {
@@ -37,7 +38,6 @@ app.use(store);
 app.use(i18n);
 app.use(globalComponents);
 app.use(directive);
-
 
 app.mount('#app');
 
