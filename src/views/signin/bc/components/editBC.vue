32e2eb9595e3580ebab6648773c6556ee0ade71e
qc200 <qc2003020403@163.com>
2024-07-21 17:55:26 +0800

    v3.2.2.8 --ADD:连班前端实现 todo:考勤数据统计相关

--- src/views/signin/bc/components/editBC.vue
+++ src/views/signin/bc/components/editBC.vue
@@ -1,78 +1,89 @@
 <script lang="ts" setup>
-import {computed, reactive} from 'vue';
-import {editBc} from '@/api/signin';
-import {Message} from '@arco-design/web-vue';
+  import { computed, reactive } from 'vue';
+  import { editBc } from '@/api/signin';
+  import { Message } from '@arco-design/web-vue';
 
-const props = defineProps({
-  signin: null,
-});
-const emit = defineEmits(["success"])
+  const props = defineProps({
+    signin: null,
+  });
+  const emit = defineEmits(['success']);
 
-if (props?.signin === null) {
-  Message.error('参数异常');
-}
-
-const bcForm = reactive({
-  id: '',
-  name: '',
-  everyDay: 1,
-  rules: [
-    {
-      count: 1,
-      sbTime: '',
-      sbStartTime: 30,
-      sbEndTime: 0,
-      xbTime: '',
-      xbStartTime: 30,
-      xbEndTime: 60,
-      ciRi: 0,
-    },
-  ],
-});
-const signin = computed(() => {
-  return props.signin;
-});
-bcForm.id = signin.value.id;
-bcForm.name = signin.value.name;
-bcForm.everyDay = signin.value.everyDay;
-bcForm.rules = signin.value.rules;
-
-const everyDayChange = (newC, OldC) => {
-  const needCount = newC;
-
-  const newList = [];
-  for (let i = 1; i <= needCount; i += 1) {
-    const moban = {
-      count: i,
-      sbTime: '',
-      sbStartTime: 30,
-      sbEndTime: 0,
-      xbTime: '',
-      xbStartTime: 30,
-      xbEndTime: 60,
-      ciRi: 0,
-    };
-    newList.push(moban);
+  if (props?.signin === null) {
+    Message.error('参数异常');
   }
-  bcForm.rules = newList;
-  console.log(bcForm);
-};
 
-const updateBC = async () => {
-  const data = await editBc(bcForm);
-  Message.success(data.msg);
-  emit("success")
-};
+  const bcForm = reactive({
+    id: '',
+    name: '',
+    everyDay: 1,
+    rules: [
+      {
+        count: 1,
+        sbTime: '',
+        sbStartTime: 30,
+        sbEndTime: 0,
+        xbTime: '',
+        xbStartTime: 30,
+        xbEndTime: 60,
+        ciRi: 0,
+        lianban: false,
+      },
+    ],
+  });
+  const signin = computed(() => {
+    return props.signin;
+  });
+  bcForm.id = signin.value.id;
+  bcForm.name = signin.value.name;
+  bcForm.everyDay = signin.value.everyDay;
+  bcForm.rules = signin.value.rules;
+
+  const everyDayChange = (newC, OldC) => {
+    const needCount = newC;
+
+    const newList = [];
+    for (let i = 1; i <= needCount; i += 1) {
+      const moban = {
+        count: i,
+        sbTime: '',
+        sbStartTime: 30,
+        sbEndTime: 0,
+        xbTime: '',
+        xbStartTime: 30,
+        xbEndTime: 60,
+        ciRi: 0,
+        lianban: false,
+      };
+      newList.push(moban);
+    }
+    bcForm.rules = newList;
+    console.log(bcForm);
+  };
+
+  const updateBC = async () => {
+    const data = await editBc(bcForm);
+    Message.success(data.msg);
+    emit('success');
+  };
+
+  const chanjiLianban = (count:number) => {
+    if (bcForm.rules[count - 1].lianban){
+      // 开
+      bcForm.rules[count].sbStartTime = 1
+      bcForm.rules[count].sbEndTime = 1
+    }else {
+      // 关
+    }
+  }
 </script>
 
 <template>
   <a-card class="add-bc">
     <div class="title-info">
       <a-tag
-      >请注意，班次的上下班时间必须顺着时间轴，上一班的下班时间必须早于本班的上班时间。每个班次内的上下班时间也必须满足顺序，否则会导致打卡失败!
+        >请注意，班次的上下班时间必须顺着时间轴，上一班的下班时间必须早于本班的上班时间。每个班次内的上下班时间也必须满足顺序，否则会导致打卡失败!
       </a-tag>
-    </div
-    >
+    </div>
     <div class="box-card">
       <div>
         <a-form
@@ -115,11 +126,13 @@ const updateBC = async () => {
               class="time-card"
             >
               <div> 第{{ item.count }}班</div>
+              <a-tag color="red" v-if="item.count!==1&&bcForm.rules[item.count - 2].lianban">此班次的上班与上一班次的下班进行连班!下班的时候将自动上班！</a-tag>
               <a-form-item
                 :rules="[{ required: true, message: '请设置上班时间' }]"
                 :validate-trigger="['change', 'blur']"
                 label="上班时间"
               >
+
                 <a-time-picker
                   v-model="bcForm.rules[item.count - 1].sbTime"
                   :disabled-seconds="
@@ -138,6 +151,8 @@ const updateBC = async () => {
                 :rules="[{ required: true, message: '必填,不允许就填0' }]"
                 :validate-trigger="['change', 'blur']"
                 label="上班前多少分钟允许打卡"
+                :disabled="item.count!==1&&bcForm.rules[item.count - 2].lianban"
+
               >
                 <a-input-number
                   v-model="bcForm.rules[item.count - 1].sbStartTime"
@@ -153,6 +168,8 @@ const updateBC = async () => {
                 ]"
                 :validate-trigger="['change', 'blur']"
                 label="上班后多少分钟允许打卡[算迟到，不算旷]"
+                :disabled="item.count!==1&&bcForm.rules[item.count - 2].lianban"
+
               >
                 <a-input-number
                   v-model="bcForm.rules[item.count - 1].sbEndTime"
@@ -230,6 +247,17 @@ const updateBC = async () => {
                 >
                 </a-input-number>
               </a-form-item>
+              <a-form-item
+                v-if="item.count !== bcForm.rules.length"
+                label="连班"
+              >
+                <a-switch
+                  v-model="bcForm.rules[item.count - 1].lianban"
+                  :checked-value="1"
+                  @change="chanjiLianban(item.count)"
+                  :unchecked-value="0"
+                />
+              </a-form-item>
             </div>
           </div>
         </a-form>
@@ -243,29 +271,29 @@ const updateBC = async () => {
 </template>
 
 <style scoped>
-.add-bc {
-  padding: 10px 10px 10px 10px;
-  border: 1px #f0f0f0 solid;
-  background-color: #ffffff;
-}
+  .add-bc {
+    padding: 10px 10px 10px 10px;
+    border: 1px #f0f0f0 solid;
+    background-color: #ffffff;
+  }
 
-.box-card {
-  display: flex;
-  flex-direction: column;
-}
+  .box-card {
+    display: flex;
+    flex-direction: column;
+  }
 
-.title {
-  border-bottom: #f0f0f0 1px solid;
-  font-size: 20px;
-}
+  .title {
+    border-bottom: #f0f0f0 1px solid;
+    font-size: 20px;
+  }
 
-.button_group {
-}
+  .button_group {
+  }
 
-.time-card {
-  width: 400px;
-  border: 1px #0085f1 solid;
-  padding: 3px 3px 3px 3px;
-  margin-left: 10px;
-}
+  .time-card {
+    width: 400px;
+    border: 1px #0085f1 solid;
+    padding: 3px 3px 3px 3px;
+    margin-left: 10px;
+  }
 </style>
